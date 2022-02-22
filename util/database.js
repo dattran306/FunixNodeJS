f69deebafde60a7bc930a9791900478cf1fadffc
Dat Tran <dattran306@gmail.com>
2022-02-22 22:20:11 +0700

    Tong ket bai hoc ket noi co so du lieu

--- util/database.js
+++ util/database.js
@@ -1,14 +1,29 @@
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
+
+let _db;
+
 const mongoConnect = (callback) => {
   MongoClient.connect(
-    "mongodb+srv://dattran306:tiendat306@cluster0.tgrlm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    "mongodb+srv://dattran306:tiendat306@cluster0.tgrlm.mongodb.net/shop?retryWrites=true&w=majority"
   )
     .then((client) => {
       console.log("Connected");
-      callback(client);
+      _db = client.db();
+      callback();
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No database found";
 };
 
 module.exports = mongoConnect;
+exports.getDb = getDb;
