2253097cfd915cc6abb570f7bef1743b35f92af5
Dat Tran <dattran306@gmail.com>
2022-02-22 22:04:49 +0700

    Cai dat trinh dieu khien MongoDB

--- util/database.js
+++ util/database.js
@@ -1,8 +1,14 @@
-const Sequelize = require("sequelize");
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://dattran306:tiendat306@cluster0.tgrlm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  )
+    .then((client) => {
+      console.log("Connected");
+      callback(client);
+    })
+    .catch((err) => console.log(err));
+};
 
-const sequelize = new Sequelize("node_complete", "root", "", {
-  dialect: "mysql",
-  host: "localhost",
-});
-
-module.exports = sequelize;
+module.exports = mongoConnect;
